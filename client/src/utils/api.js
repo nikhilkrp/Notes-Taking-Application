ea2d794ca9e090b7b0d8875898339f6a9a5115c6
nikhilkrp <150312021+nikhilkrp@users.noreply.github.com>
2025-09-30 00:15:02 +0530

    test route

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://notes-taking-application-64zj.onrender.com/api",
+  baseURL: "http://localhost:5000/api",
 });
 
 API.interceptors.request.use(config => {
--- server/server.js
+++ server/server.js
@@ -12,11 +12,15 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: "https://notes-taking-application-git-main-nikhils-projects-9dc5e1f8.vercel.app/",
+  origin: "http://localhost:5173",
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"], 
   credentials:true,
 }));
+
+app.get("/", (req, res) => {
+  res.json({ message: "Backend is working!" });
+});
 
 app.use("/api/auth", authRoutes);
 app.use("/api/notes", noteRoutes);
